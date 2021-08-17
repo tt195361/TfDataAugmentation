4bb668f93d8e123970decd29021f812b7b3dc10c
tt195361 <tt195361@gmail.com>
2021-08-17 20:01:14 +0900

    OneOf.py: changed to use eval().

--- TfDataAugmentation/OneOf.py
+++ TfDataAugmentation/OneOf.py
@@ -2,6 +2,7 @@
 # OneOf.py
 #
 
+import tensorflow as tf
 from . import BaseAug
 from . import gen_utils
 
@@ -11,11 +12,29 @@ class OneOf(BaseAug):
         super(OneOf, self).__init__(p)
         self.transforms = gen_utils.check_transforms(transforms)
         self.n_transforms = len(transforms)
+        self.select_transform_method = eval(
+            self.make_select_transform_method(self.n_transforms))
+
+    def make_select_transform_method(self, n_transforms):
+        s = \
+            "lambda idx, transforms: " \
+            "    tf.switch_case(" \
+            "        idx," \
+            "        branch_fns={"
+        for i in range(n_transforms):
+            s += "{0}: lambda: transforms[{0}]".format(i)
+            if i < n_transforms - 1:
+                s += ","
+        s += \
+            "        }" \
+            "    )"
+        return s
 
     def __call__(self, **data):
         idx = gen_utils.random_int(
             shape=[], minval=0, maxval=self.n_transforms)
-        selected_transform = self.transforms[idx]
+        selected_transform = \
+            self.select_transform_method(idx, self.transforms)
         data = selected_transform(**data)
         return data
 
