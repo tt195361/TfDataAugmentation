ed069a493b82621a5203120f9d60c611f079c59f
tt195361 <tt195361@gmail.com>
2021-09-20 12:08:36 +0900

    Changed MedianBlur implementation, write explicit program instead of dynamically generated program. Generated program caused error on Kaggle...

--- TfDataAugmentation/MedianBlur.py
+++ TfDataAugmentation/MedianBlur.py
@@ -3,13 +3,14 @@
 #
 
 import tensorflow as tf
-import tensorflow_addons as tfa # noqa: for PyCharm inspection
+import tensorflow_addons as tfa
 from . import BaseAug
 from . import gen_utils
 
 
 class MedianBlur(BaseAug):
     MIN_KSIZE = 3
+    MAX_KSIZE = 10
 
     def __init__(
             self,
@@ -17,25 +18,7 @@ class MedianBlur(BaseAug):
             p=0.5):
         super(MedianBlur, self).__init__(p)
         self.blur_limit = gen_utils.check_int_range(
-            blur_limit, self.MIN_KSIZE, None, "blur_limit")
-        # ksize for tf.image.median_filter2d must be integer,
-        # not tensor, so needs some program...
-        self.median_filter2d_method = eval(
-            self.make_median_filter2d_method())
-
-    def make_median_filter2d_method(self):
-        s = "lambda ksize, image: "
-        s += "   tf.switch_case("
-        s += "       ksize, "
-        s += "       branch_fns={"
-        # branch index must start from 0
-        for i in range(0, self.blur_limit + 1):
-            s += "      {0}: lambda: tfa.image.median_filter2d(image, filter_shape=({0}, {0}))".format(i)
-            if i < self.blur_limit:
-                s += ", "
-        s += "       }"
-        s += "   )"
-        return s
+            blur_limit, self.MIN_KSIZE, self.MAX_KSIZE, "blur_limit")
 
     def _make_params(self):
         ksize = gen_utils.random_int(
@@ -48,7 +31,32 @@ class MedianBlur(BaseAug):
     def _do_aug_image(self, image):
         image_shape = tf.shape(image)
         ksize = self.params['ksize']
-        aug_image = \
-            self.median_filter2d_method(ksize, image)
+
+        # filter_shape for tf.image.median_filter2d must be integer,
+        # not tensor, so needs some program...
+        def median_filter2d(filter_shape):
+            def _do_filter():
+                filter_image = tfa.image.median_filter2d(
+                    image, filter_shape=filter_shape)
+                return filter_image
+            return _do_filter
+
+        aug_image = tf.switch_case(
+            ksize,
+            branch_fns={
+                # branch index must start from 0
+                0: median_filter2d(0),
+                1: median_filter2d(1),
+                2: median_filter2d(2),
+                3: median_filter2d(3),
+                4: median_filter2d(4),
+                5: median_filter2d(5),
+                6: median_filter2d(6),
+                7: median_filter2d(7),
+                8: median_filter2d(8),
+                9: median_filter2d(9),
+                10: median_filter2d(10)
+            }
+        )
         aug_image = tf.reshape(aug_image, image_shape)
         return aug_image
