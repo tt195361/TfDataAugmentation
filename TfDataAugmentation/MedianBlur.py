76b010640e6466b48cb4d3fdb8297623122e3c2a
tt195361 <tt195361@gmail.com>
2021-09-20 15:15:06 +0900

    For debug, call median_filter2d directly without using tf.switch_case

--- TfDataAugmentation/MedianBlur.py
+++ TfDataAugmentation/MedianBlur.py
@@ -29,35 +29,37 @@ class MedianBlur(BaseAug):
         return params
 
     def _do_aug_image(self, image):
-        # filter_shape for tf.image.median_filter2d must be integer,
-        # not tensor, so needs some program...
-        def median_filter(ksz):
-            filter_shape = (ksz, ksz)
-            def _do_filter():
-                filter_image = median_filter2d(
-                    image, filter_shape=filter_shape)
-                return filter_image
-            return _do_filter
-
+        # # filter_shape for tf.image.median_filter2d must be integer,
+        # # not tensor, so needs some program...
+        # def median_filter(ksz):
+        #     filter_shape = (ksz, ksz)
+        #     def _do_filter():
+        #         filter_image = median_filter2d(
+        #             image, filter_shape=filter_shape)
+        #         return filter_image
+        #     return _do_filter
+        #
         image_shape = tf.shape(image)
-        ksize = self.params['ksize']
-        aug_image = tf.switch_case(
-            ksize,
-            branch_fns={
-                # branch index must start from 0
-                0: median_filter(0),
-                1: median_filter(1),
-                2: median_filter(2),
-                3: median_filter(3),
-                4: median_filter(4),
-                5: median_filter(5),
-                6: median_filter(6),
-                7: median_filter(7),
-                8: median_filter(8),
-                9: median_filter(9),
-                10: median_filter(10)
-            }
-        )
+        # ksize = self.params['ksize']
+        # aug_image = tf.switch_case(
+        #     ksize,
+        #     branch_fns={
+        #         # branch index must start from 0
+        #         0: median_filter(0),
+        #         1: median_filter(1),
+        #         2: median_filter(2),
+        #         3: median_filter(3),
+        #         4: median_filter(4),
+        #         5: median_filter(5),
+        #         6: median_filter(6),
+        #         7: median_filter(7),
+        #         8: median_filter(8),
+        #         9: median_filter(9),
+        #         10: median_filter(10)
+        #     }
+        # )
+
+        aug_image = median_filter2d(image, filter_shape=(5, 5))
         aug_image = tf.reshape(aug_image, image_shape)
         return aug_image
 
