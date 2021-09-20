ced8645804f071227789f1d78b1e82b22bda52fc
tt195361 <tt195361@gmail.com>
2021-09-20 16:32:17 +0900

    Debug: changed tf.switch_case to python if ... elif ... else.

--- TfDataAugmentation/MedianBlur.py
+++ TfDataAugmentation/MedianBlur.py
@@ -41,23 +41,24 @@ class MedianBlur(BaseAug):
         #
         image_shape = tf.shape(image)
         ksize = self.params['ksize']
-        aug_image = tf.switch_case(
-            ksize,
-            branch_fns={
-                # branch index must start from 0
-                0: lambda: tfa.image.median_filter2d(image, filter_shape=[0, 0]),
-                1: lambda: tfa.image.median_filter2d(image, filter_shape=[1, 1]),
-                2: lambda: tfa.image.median_filter2d(image, filter_shape=[2, 2]),
-                3: lambda: tfa.image.median_filter2d(image, filter_shape=[3, 3]),
-                4: lambda: tfa.image.median_filter2d(image, filter_shape=[4, 4]),
-                5: lambda: tfa.image.median_filter2d(image, filter_shape=[5, 5]),
-                6: lambda: tfa.image.median_filter2d(image, filter_shape=[6, 6]),
-                7: lambda: tfa.image.median_filter2d(image, filter_shape=[7, 7]),
-                8: lambda: tfa.image.median_filter2d(image, filter_shape=[8, 8]),
-                9: lambda: tfa.image.median_filter2d(image, filter_shape=[9, 9]),
-                10: lambda: tfa.image.median_filter2d(image, filter_shape=[10, 10])
-            }
-        )
+
+        if ksize == 3:
+            aug_image = tfa.image.median_filter2d(image, filter_shape=[3, 3])
+        elif ksize == 4:
+            aug_image = tfa.image.median_filter2d(image, filter_shape=[4, 4])
+        elif ksize == 5:
+            aug_image = tfa.image.median_filter2d(image, filter_shape=[5, 5])
+        elif ksize == 6:
+            aug_image = tfa.image.median_filter2d(image, filter_shape=[6, 6])
+        elif ksize == 7:
+            aug_image = tfa.image.median_filter2d(image, filter_shape=[7, 7])
+        elif ksize == 8:
+            aug_image = tfa.image.median_filter2d(image, filter_shape=[8, 8])
+        elif ksize == 9:
+            aug_image = tfa.image.median_filter2d(image, filter_shape=[9, 9])
+        else:
+            aug_image = tfa.image.median_filter2d(image, filter_shape=[10, 10])
+
 
         aug_image = tf.reshape(aug_image, image_shape)
         return aug_image
