b8771f32df5acd641cf3dd2d7d0c2d53058adeed
tt195361 <tt195361@gmail.com>
2021-09-21 13:30:39 +0900

    Added HueSaturationValue

---
+++ TfDataAugmentation/HueSaturationValue.py
@@ -0,0 +1,53 @@
+#
+# HueSaturationValue.py
+#
+
+import tensorflow as tf
+from . import BaseAug
+from . import gen_utils
+
+
+class HueSaturationValue(BaseAug):
+    def __init__(
+            self,
+            hue_shift_limit=20,
+            sat_shift_limit=30,
+            val_shift_limit=20,
+            p=0.5):
+        super(HueSaturationValue, self).__init__(p)
+        self.hue_shift_limit = gen_utils.check_float_range(
+            hue_shift_limit, 0, None, "hue_shift_limit")
+        self.sat_shift_limit = gen_utils.check_float_range(
+            sat_shift_limit, 0, None, "sat_shift_limit")
+        self.val_shift_limit = gen_utils.check_float_range(
+            val_shift_limit, 0, None, "val_shift_limit")
+
+    def _make_params(self):
+        hue_shift = gen_utils.random_float(
+            [], -self.hue_shift_limit, self.hue_shift_limit)
+        sat_shift = gen_utils.random_float(
+            [], -self.sat_shift_limit, self.sat_shift_limit)
+        val_shift = gen_utils.random_float(
+            [], -self.val_shift_limit, self.val_shift_limit)
+        params = {
+            "hue_shift": hue_shift,
+            "sat_shift": sat_shift,
+            "val_shift": val_shift,
+        }
+        return params
+
+    def _do_aug_image(self, image):
+        hue_shift = self.params['hue_shift'] / 360.0
+        sat_shift = self.params['sat_shift']
+        val_shift = self.params['val_shift']
+
+        hsv_image = tf.image.rgb_to_hsv(image)
+        hue_values = (hsv_image[..., 0] + hue_shift) % 1.0
+        sat_values = hsv_image[..., 1] + sat_shift
+        sat_values = tf.clip_by_value(sat_values, 0.0, 1.0)
+        val_values = hsv_image[..., 2] + val_shift
+        val_values = tf.clip_by_value(val_values, 0.0, 1.0)
+        hsv_image = tf.stack(
+            [hue_values, sat_values, val_values], axis=-1)
+        aug_image = tf.image.hsv_to_rgb(hsv_image)
+        return aug_image
---
+++ tests/test_HueSaturationValue.py
@@ -0,0 +1,97 @@
+#
+# test_HueSaturationValue.py
+#
+
+import pytest
+import albumentations as A
+from .context import TfDataAugmentation as Tfda
+from . import test_utils
+from .test_utils import TestResult
+
+
+@pytest.mark.parametrize(
+    "hue_shift_limit, expected, message", [
+        (-1, TestResult.Error, "< min => Error"),
+        (0, TestResult.OK, "== min => OK"),
+        (10000, TestResult.OK, "no max limit => OK"),
+    ])
+def test_hue_shift_limit_value(
+        hue_shift_limit, expected, message):
+    try:
+        Tfda.HueSaturationValue(
+            hue_shift_limit=hue_shift_limit,
+            sat_shift_limit=0,
+            val_shift_limit=0)
+        actual = TestResult.OK
+    except ValueError:
+        actual = TestResult.Error
+    assert expected == actual, message
+
+
+@pytest.mark.parametrize(
+    "sat_shift_limit, expected, message", [
+        (-1, TestResult.Error, "< min => Error"),
+        (0, TestResult.OK, "== min => OK"),
+        (10000, TestResult.OK, "no max limit => OK"),
+    ])
+def test_sat_shift_limit_value(
+        sat_shift_limit, expected, message):
+    try:
+        Tfda.HueSaturationValue(
+            hue_shift_limit=0,
+            sat_shift_limit=sat_shift_limit,
+            val_shift_limit=0)
+        actual = TestResult.OK
+    except ValueError:
+        actual = TestResult.Error
+    assert expected == actual, message
+
+
+@pytest.mark.parametrize(
+    "val_shift_limit, expected, message", [
+        (-1, TestResult.Error, "< min => Error"),
+        (0, TestResult.OK, "== min => OK"),
+        (10000, TestResult.OK, "no max limit => OK"),
+    ])
+def test_val_shift_limit_value(
+        val_shift_limit, expected, message):
+    try:
+        Tfda.HueSaturationValue(
+            hue_shift_limit=0,
+            sat_shift_limit=0,
+            val_shift_limit=val_shift_limit)
+        actual = TestResult.OK
+    except ValueError:
+        actual = TestResult.Error
+    assert expected == actual, message
+
+
+@pytest.mark.parametrize(
+    "hue_shift_limit, sat_shift_limit, val_shift_limit", [
+        (90, 0,  0),
+        (0, 20,  0),
+        (0,  0, 30),
+    ])
+def test_call(
+        hue_shift_limit, sat_shift_limit, val_shift_limit):
+    tgt_hsv = Tfda.HueSaturationValue(
+        hue_shift_limit=hue_shift_limit,
+        sat_shift_limit=sat_shift_limit,
+        val_shift_limit=val_shift_limit,
+        p=1.0)
+    tgt_transform = \
+        test_utils.make_tgt_transform(tgt_hsv)
+    image = test_utils.make_test_image()
+
+    tgt_result = tgt_transform(image=image)
+    actual_image = tgt_result['image']
+
+    image_np = image.numpy()
+    hue_shift = float(tgt_hsv.get_param('hue_shift'))
+    sat_shift = float(tgt_hsv.get_param('sat_shift'))
+    val_shift = float(tgt_hsv.get_param('val_shift'))
+    expected_image = A.shift_hsv(
+        image_np, hue_shift, sat_shift, val_shift)
+
+    test_utils.assert_array(
+        expected_image, actual_image, "image")
